756990be2bddccdc458da05d28bab1f645a3d893
Christian Newkirk <christiannewkirk@umail.ucsb.edu>
2016-02-11 18:52:48 -0800

    First attempt at Animation

---
+++ src/edu/ucsb/cs56/w16/drawings/christiannewkirk/advanced/AnimatedPictureViewer.java
@@ -0,0 +1,99 @@
+package edu.ucsb.cs56.w16.drawings.christiannewkirk.advanced;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * A main class to view an animation
+ *
+ * @author Christian Newkirk
+ * @version for CS56, W16
+ */
+
+
+public class AnimatedPictureViewer {
+
+    private DrawPanel panel = new DrawPanel();
+    
+    private IPad iPad = new IPad(100, 100, 200, 253);
+    
+    Thread anim;   
+    
+    private int x = 170;
+    private int y = 160;
+    
+    private int dx = 5;
+
+    public static void main (String[] args) {
+      new AnimatedPictureViewer().go();
+    }
+
+    public void go() {
+      JFrame frame = new JFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      frame.getContentPane().add(panel);
+      frame.setSize(640,480);
+      frame.setVisible(true);
+      
+      frame.getContentPane().addMouseListener(new MouseAdapter() {
+        public void mouseEntered(MouseEvent e){
+        System.out.println("mouse entered");
+          anim = new Animation();
+          anim.start();
+        }
+
+        public void mouseExited(MouseEvent e){        
+          System.out.println("Mouse exited");
+          // Kill the animation thread
+          anim.interrupt();
+          while (anim.isAlive()){}
+          anim = null;         
+          panel.repaint();        
+        }
+      });
+      
+    } // go()
+
+    class DrawPanel extends JPanel {
+       public void paintComponent(Graphics g) {
+
+        Graphics2D g2 = (Graphics2D) g;
+
+         // Clear the panel first
+          g2.setColor(Color.white);
+          g2.fillRect(0,0,this.getWidth(), this.getHeight());
+
+          // Draw the Ipod
+          g2.setColor(Color.BLACK);
+          IPad test = new IPad(x, y, 200, 253);
+          g2.draw(test);
+       }
+    }
+    
+    class Animation extends Thread {
+      public void run() {
+        try {
+          while (true) {
+            // Bounce off the walls
+
+            if (y >= 194) { dx = -5; }
+            if (y <= 0) { dx = 5; }
+            
+            y += dx;                
+            panel.repaint();
+            Thread.sleep(50);
+          }
+        } catch(Exception ex) {
+          if (ex instanceof InterruptedException) {
+            // Do nothing - expected on mouseExited
+          } else {
+            ex.printStackTrace();
+            System.exit(1);
+          }
+        }
+      }
+    }
+    
+}
